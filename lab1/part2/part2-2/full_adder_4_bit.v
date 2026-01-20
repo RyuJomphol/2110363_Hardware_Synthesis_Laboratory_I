0a72d30c0f2e7c7833c4be1eb74349667b4a7fe6
RyuJomphol <jomphol2548@gmail.com>
2026-01-20 18:25:25 +0700

    edit md + pre_lab2

--- lab1/part2/part2-2/full_adder_4_bit.v
+++ lab1/part2/part2-2/full_adder_4_bit.v
@@ -6,23 +6,11 @@ module full_adder_4_bit (
     output wire cout
 );
     // TODO: Implement the 4-bit full adder here
-    wire c1, c2, c3;
+    wire [4:0] tmp;
 
-    // bit 0 (LSB)
-    assign sum[0] = a[0] ^ b[0] ^ cin;
-    assign c1     = (a[0] & b[0]) | (a[0] & cin) | (b[0] & cin);
-
-    // bit 1
-    assign sum[1] = a[1] ^ b[1] ^ c1;
-    assign c2     = (a[1] & b[1]) | (a[1] & c1) | (b[1] & c1);
-
-    // bit 2
-    assign sum[2] = a[2] ^ b[2] ^ c2;
-    assign c3     = (a[2] & b[2]) | (a[2] & c2) | (b[2] & c2);
-
-    // bit 3 (MSB)
-    assign sum[3] = a[3] ^ b[3] ^ c3;
-    assign cout   = (a[3] & b[3]) | (a[3] & c3) | (b[3] & c3);
+    assign tmp  = {1'b0, a} + {1'b0, b} + cin;
+    assign sum  = tmp[3:0];
+    assign cout = tmp[4];
 
     
 endmodule
